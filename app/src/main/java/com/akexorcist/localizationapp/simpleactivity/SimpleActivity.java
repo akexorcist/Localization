a003c0864b80bf25c516678f70c91a42cb3bbacf
Akexorcist <akexorcist@gmail.com>
2015-07-23 12:28:52 +0700

    Remove unused

--- app/src/main/java/com/akexorcist/localizationapp/simpleactivity/SimpleActivity.java
+++ app/src/main/java/com/akexorcist/localizationapp/simpleactivity/SimpleActivity.java
@@ -1,10 +1,6 @@
 package com.akexorcist.localizationapp.simpleactivity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.HorizontalScrollView;
 
