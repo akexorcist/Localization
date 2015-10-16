fd2bc6d043a1edb1925880602dcc8442d4ac1edd
Akexorcist <akexorcist@gmail.com>
2015-10-16 13:02:43 +0700

    Add blank dummy activity

---
+++ localizationActivity/src/main/java/com/akexorcist/localizationactivity/BlankDummyActivity.java
@@ -0,0 +1,30 @@
+package com.akexorcist.localizationactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class BlankDummyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(R.anim.animation_localization_activity_transition_in,
+                R.anim.animation_localization_activity_transition_out);
+        setContentView(R.layout.activity_blank_dummy);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 200);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.animation_localization_activity_transition_in,
+                R.anim.animation_localization_activity_transition_out);
+    }
+}
