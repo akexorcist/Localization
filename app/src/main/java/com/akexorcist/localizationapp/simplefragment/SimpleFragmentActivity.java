2ab9f764436229528b7823bf08a602749da19934
Nuwan Chamara <nuwanchamaramail@gmail.com>
2019-12-10 11:10:14 +0530

    androidx migrate

--- app/src/main/java/com/akexorcist/localizationapp/simplefragment/SimpleFragmentActivity.java
+++ app/src/main/java/com/akexorcist/localizationapp/simplefragment/SimpleFragmentActivity.java
@@ -1,8 +1,8 @@
 package com.akexorcist.localizationapp.simplefragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import android.view.View;
 import android.widget.HorizontalScrollView;
 
